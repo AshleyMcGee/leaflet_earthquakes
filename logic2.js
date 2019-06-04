b1a440de3ba8aea38737168c21818990dcb6c9b4
Ashley McGee <6312276+AshleyMcGee@users.noreply.github.com>
2019-06-04 14:23:10 -0500

    Finishing touches to earthquake map

--- logic2.js
+++ logic2.js
@@ -1,38 +1,6 @@
 
 (async function(){
 
-    const API_KEY = "pk.eyJ1IjoiYXNobGV5bWNnZWUiLCJhIjoiY2p2b2Zwcjg4MDByMzQ5cGZyNzJ6OGx2ZSJ9.Cw5sL5cs6-tPsqHY1Dc2kQ"
-    const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
-    const response = await d3.json(url)
-    console.log(response);
-    console.log([response.features[0].geometry.coordinates[1], response.features[0].geometry.coordinates[0]]);
-
-    /*
-    function setColor(magnitude){
-
-      
-        if(magnitude >= 5.0){
-            return "red"
-        }
-        else if(magnitude < 5.0 && magnitude > 4.0){
-            return "orange"
-        }
-        else if(magnitude < 4.0 && magnitude > 3.0){
-            return "yellow"
-        }
-        else if(magnitude < 3.0 && magnitude > 2.0){
-          return "blue"
-        }
-        else if(magnitude < 2.0 && magnitude > 1.0){
-          return "green"
-        }
-        else{
-            return "black"
-        }
-        
-
-    }*/
-
     function setColor(d) {
       return d > 6.0 ? '#7a0177' :
       d > 5.0 ? '#BD0026' :
@@ -40,22 +8,14 @@
       d > 3.0 ? '#FC4E2A' :
       d > 2.0 ? '#FD8D3C' :
       d > 1.0 ? '#FEB24C' :
-      '#FFEDA0';
+      d > 0.0 ? '#FFEDA0' :
+      '#696969';
       }
 
-    const myMap = L.map("map", {
-      center: [15.5994, -28.6731],
-      zoom: 3
-    });
-    
-    L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.streets-basic",
-      accessToken: API_KEY
-    }).addTo(myMap);
+  function createFeatures(earthquakeData) {
 
-    function options(features){
+      //Style for the circleMarker features
+      function options(features){
 
         return {
 
@@ -64,44 +24,103 @@
           fillOpacity: 0.75,
           color: "none",
 
-        }
-    }
+          }
+      }
 
+    //Bind the information from the data to the popups
     function onEachFeature(feature,layer){
 
       layer.bindPopup("<h3>" + feature.properties.place + "</h3>" + "<hr><ul><li>" + feature.properties.mag + "</li>" + "<li>" + new Date(feature.properties.time) + "</li></ul>")
+      }
+
+      //Create the geojson layer with circlMarkers
+      const earthquakes = L.geoJson(earthquakeData, {
+
+          pointToLayer: function(feature, latlng){
+            return L.circleMarker(latlng, options(feature))
+          },
+          onEachFeature: onEachFeature
+      })
+
+      createMap(earthquakes)
+
   }
 
-    L.geoJson(response, {
+  function createMap(earthquakes){
 
-        pointToLayer: function(feature, latlng){
-          return L.circleMarker(latlng, options(feature))
-        },
-        onEachFeature: onEachFeature
-    }).addTo(myMap)
+        //First tile layer for streets basic
+        let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+          maxZoom: 18,
+          id: "streets-v9",
+          accessToken: API_KEY
+        })
 
-    
-    const legend = L.control({position: 'bottomright'});
-    legend.onAdd = function (map) {
+        //Second tile layer for topography
+        let satellites = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+          maxZoom: 18,
+          id: "satellite-streets-v9",
+          accessToken: API_KEY 
+        })
 
-      const div = L.DomUtil.create('div', 'info legend');
-      labels = ['<strong>Magnitude</strong>'],
-      categories = ['Greater than 5.0','Between 4.0 and 5.0','Between 2.0 and 3.0','Between 1.0 and 2.0', "Less than 1.0"];
+        //Third tile layer for boundaries
+        
+        //Create basemaps
+        const baseMaps = {
 
-      for (var i = 0; i < categories.length; i++) {
-
-              div.innerHTML += 
-              labels.push(
-                  '<i style="background:' + setColor(categories[i]) + '"></i> ' +
-              (categories[i] ? categories[i] : '+'));
-
-              }
-
-          div.innerHTML = labels.join('<br>');
-
-      return div;
+          "Satellite": satellites,
+          "Streets": streets
+          
       };
+
+      // Create overlay object to hold our overlay layer
+      const overlayMaps = {
+      Earthquakes: earthquakes
+};
+
+        //Create the map itself
+        const myMap = L.map("map", {
+        center: [15.5994, -28.6731],
+        zoom: 3,
+        layers: [satellites, earthquakes]
+          });
+
+      // Pass our map layers into our layer control
+      // Add the layer control to the map
+      L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+        }).addTo(myMap);
+
+      //create the legend
+      const legend = L.control({position: 'bottomright'});
+
+      legend.onAdd = function (map) {
+
+      let div = L.DomUtil.create('div', 'info legend'),
+        grades = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+        labels = [];
+
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + setColor(grades[i] + 1) + '"></i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+          return div;
+        };
+
       legend.addTo(myMap);
-    
+
+  }
+
+
+    //Get the data
+    const API_KEY = "pk.eyJ1IjoiYXNobGV5bWNnZWUiLCJhIjoiY2p3ZXpvczdwMW1ndDRicW45azY0b2EwdiJ9.fqOkJmG3F4VrEJ5Ksnhp8Q"
+    const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+    const response = await d3.json(url)
+    console.log(response);
+    createFeatures(response.features)
 
   })()
